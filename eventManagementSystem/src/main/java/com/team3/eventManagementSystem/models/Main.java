6ada0c0162a42d1e650aace483a8f0ee16a2dd3c
eleniKouv <108596410+eleniKouv@users.noreply.github.com>
2025-01-06 18:22:06 +0200

    correct main 2

--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/models/Main.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/models/Main.java
@@ -9,14 +9,17 @@ public class Main {
 
 		Organizer org1=new Organizer("Eleni", "Kouvalakidou", " elenik@gmail.com" , 12345,"myDescription ");
         Event event1=new Event( "latin Night", "dancing", "best night", 100, 02, 23, 0, 230, org1);
-        ApprovalRequest aRequest=new ApprovalRequest("type",event1, org1, "comments");
-        org1.makeApproveRequest(aRequest);
-        //ApprovalRequest request1= org1.makeApproveRequest(" requestDetails", event1, "myComments");
-        //ApprovalRequest request2= org1.makeApproveRequest(" requestDetails2", event1, "myComments2");
+       
+       // ApprovalRequest aRequest=new ApprovalRequest("type",event1, org1, "comments");
+        ApprovalRequest request1= new ApprovalRequest(" requestDetails", event1,org1, "myComments");
+        ApprovalRequest request2= new ApprovalRequest(" requestDetails2", event1, org1, "myComments2");
+        org1.makeApproveRequest(request1);
+        org1.makeApproveRequest(request2);
         Employee emp1=new Employee("empName", "empSurname", "emp@gmail.com");
         
         //returns true if the employee has the event, false if not(e.g. belongs to other employee)
-        emp1. approveRequest(aRequest); 
+        emp1. approveRequest(request1); 
+        emp1.rejectRequest(request2);
         
         
     }
