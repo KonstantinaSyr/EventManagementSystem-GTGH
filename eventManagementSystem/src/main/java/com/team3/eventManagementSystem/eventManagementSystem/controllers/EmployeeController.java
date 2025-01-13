60a15f0ea84bc39b4b023c57ec0ed162fd5df887
eleniKouv <108596410+eleniKouv@users.noreply.github.com>
2025-01-13 21:40:41 +0200

    showEmployees implemented
    works ok!

--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/EmployeeController.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/EmployeeController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.team3.eventManagementSystem.eventManagementSystem.models.Employee;
 import com.team3.eventManagementSystem.eventManagementSystem.service.EmployeeService;
 
+import org.springframework.web.bind.annotation.GetMapping;
 //import src.main.java.com.example.demo.controllers.PostMapping;
 //import src.main.java.com.example.demo.controllers.RequestMapping;
 //import src.main.java.com.example.demo.controllers.RestController;
@@ -27,14 +27,25 @@ public class EmployeeController {
 	 Employee anEmployee= new Employee(name, surname, email);
 	 return employeeService.addEmployee(anEmployee);
  }
+ 
  /*
  @PostMapping("/addMany")
- public List<Employee> addManyEmployees(@RequestBody List<Employee> employees)
+ public List<Employee> addManyEmployees(@RequestBody List<Employee> employees){
+	 //Should create the objects employee
+	 for (Employee employee: employees) {
+		 Employee anEmployee= new Employee();
+	 }
+	 return employeeService.addManyEmployees(employees);
 }
 */
  @PostMapping("/update")
  public List<Employee> updateEmployee(@RequestParam int id, @RequestParam(required=false) String newName, @RequestParam(required=false) String newSurname, @RequestParam(required=false) String newEmail){
 	 return employeeService.updateEmployee(id, newName,newSurname, newEmail);
  }
+ 
+ @GetMapping("/show")
+	public List<Employee> showEmployees(){
+		return employeeService.getAllEmployees();
+	}
  
 }
