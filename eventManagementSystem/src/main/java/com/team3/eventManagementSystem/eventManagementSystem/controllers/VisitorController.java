f3008d1523082e53e6f40f34372c8c3c9ed87bf6
Kagia-Eleni <103891240+Kagia-Eleni@users.noreply.github.com>
2025-01-13 21:45:50 +0200

    Update VisitorController.java
    Added methods to add, delete and update a Visitor and one to get a list with all the visitors.

--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/VisitorController.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/VisitorController.java
@@ -1,5 +1,45 @@
 package com.team3.eventManagementSystem.eventManagementSystem.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team3.eventManagementSystem.eventManagementSystem.models.Visitor;
+import com.team3.eventManagementSystem.eventManagementSystem.service.VisitorService;
+
+@RestController
+@RequestMapping("visitors")
 public class VisitorController {
+
+	@Autowired
+	VisitorService visitorService;
+
+	@GetMapping("/allVisitors")
+	public List<Visitor> getAllVisitors() {
+		return visitorService.getAllVisitors();
+	}
+
+	@PostMapping("/addVisitor")
+	public List<Visitor> addVisitor(@RequestBody Visitor visitor) {
+		return visitorService.addVisitor(visitor);
+	}
+
+	@DeleteMapping("/deleteVisitor")
+	public List<Visitor> deleteVisitor(@RequestParam Integer id) {
+		return visitorService.deleteVisitor(id);
+	}
+
+	@PutMapping("/putVisitor")
+	public List<Visitor> deleteVisitor(@RequestParam Integer id, String name, String surname, String email) {
+		return visitorService.updateVisitor(id, name, surname, email);
+	}
 
 }
