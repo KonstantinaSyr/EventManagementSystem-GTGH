fd79031cc49acd43aa03155a4bb08eb3d6ab9809
Kagia-Eleni <103891240+Kagia-Eleni@users.noreply.github.com>
2025-01-13 22:09:28 +0200

    Update RequestController.java
    Added methods to add and delete a Request and methods to show all events and all events by status

--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/RequestController.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/RequestController.java
@@ -1,5 +1,44 @@
 package com.team3.eventManagementSystem.eventManagementSystem.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.team3.eventManagementSystem.eventManagementSystem.models.ApprovalRequest;
+import com.team3.eventManagementSystem.eventManagementSystem.service.RequestService;
+
+@RestController
+@RequestMapping("requests")
 public class RequestController {
+
+	@Autowired
+	RequestService requestService;
+
+	@GetMapping("/allRequests")
+	public List<ApprovalRequest> getAllRequests() {
+		return requestService.getAllRequests();
+	}
+
+	@PostMapping("/addRequest")
+	public List<ApprovalRequest> addRequest(@RequestBody ApprovalRequest request) {
+		return requestService.createRequest(request);
+	}
+
+	@DeleteMapping("/deleteRequest")
+	public List<ApprovalRequest> deleteRequest(@RequestParam Integer id) {
+		return requestService.deleteRequest(id);
+	}
+	
+	@GetMapping("/requestsByStatus")
+	public List<ApprovalRequest> requestsByStatus(@RequestParam String status) {
+		return requestService.showRequests(status);
+	}
 
 }
