8408d40f9f76d7bbe3d73ead8709af0f9e4313ae
Kagia-Eleni <103891240+Kagia-Eleni@users.noreply.github.com>
2025-01-15 21:29:21 +0200

    Fixed EmployeeController
    Added method to delete an Employee and fixed the method to put Employee

--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/EmployeeController.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/EmployeeController.java
@@ -3,8 +3,10 @@ package com.team3.eventManagementSystem.eventManagementSystem.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,20 +30,25 @@ public class EmployeeController {
 	 */
 
 	// Works for adding 1 or more Employees
-	@PostMapping("/add")
+	@PostMapping("/addEmployee")
 	public List<Employee> addEmployees(@RequestBody List<Employee> employees) {
 		return employeeService.addManyEmployees(employees);
 	}
 
-	@PostMapping("/update")
-	public List<Employee> updateEmployee(@RequestParam int id, @RequestParam(required = false) String newName,
+	@PutMapping("/updateEmployee")
+	public List<Employee> updateEmployee(@RequestParam Integer id, @RequestParam(required = false) String newName,
 			@RequestParam(required = false) String newSurname, @RequestParam(required = false) String newEmail) {
 		return employeeService.updateEmployee(id, newName, newSurname, newEmail);
 	}
 
-	@GetMapping("/show")
-	public List<Employee> showEmployees() {
+	@GetMapping("/getAllEmployees")
+	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
+	}
+	
+	@DeleteMapping("/deleteEmployee") 
+	public List<Employee> deleteEmployee(@RequestParam Integer employeeId) {
+		return employeeService.deleteEmployeeById(employeeId);
 	}
 
 }
