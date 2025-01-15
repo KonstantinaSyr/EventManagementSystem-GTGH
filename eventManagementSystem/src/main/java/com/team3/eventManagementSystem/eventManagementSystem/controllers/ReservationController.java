b0dc8bb3b8c5c543bf65801c2d16249ff2095d17
Kagia-Eleni <103891240+Kagia-Eleni@users.noreply.github.com>
2025-01-15 23:41:57 +0200

    Implemented QR Code Function
    Added methods in ReservationService and ReservationController in order to create and send QR Codes of the reservations of the visitors.

--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/ReservationController.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/controllers/ReservationController.java
@@ -1,8 +1,16 @@
 package com.team3.eventManagementSystem.eventManagementSystem.controllers;
 
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +40,28 @@ public class ReservationController {
 	@GetMapping("/reservationsOfVisitor")
 	public List<Reservation> findReservationsByVisitor(@RequestParam Integer userId) {
 		return reservationService.findReservationByVisitor(userId);
+	}
+	
+	@GetMapping("/generateQRCode")
+	public ResponseEntity<byte[]> generateQRCodeForReservation(@RequestParam Integer userId, @RequestParam Integer eventId) {
+	    try {
+	        Reservation reservation = reservationService.createReservation(userId, eventId);
+	        BufferedImage qrCodeImage = reservationService.generateQRCodeForReservation(reservation);
+
+	        // Convert BufferedImage to byte array for response
+	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	        ImageIO.write(qrCodeImage, "PNG", baos);
+	        byte[] imageBytes = baos.toByteArray();
+
+	        // Return the image in the response
+	        HttpHeaders headers = new HttpHeaders();
+	        headers.setContentType(MediaType.IMAGE_PNG);
+	        return new ResponseEntity<>(imageBytes, headers, HttpStatus.OK);
+
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+	    }
 	}
 
 }
--- eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/service/ReservationService.java
+++ eventManagementSystem/src/main/java/com/team3/eventManagementSystem/eventManagementSystem/service/ReservationService.java
@@ -1,9 +1,19 @@
 package com.team3.eventManagementSystem.eventManagementSystem.service;
 
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.team3.eventManagementSystem.eventManagementSystem.models.Reservation;
 
 @Service
@@ -13,12 +23,13 @@ public class ReservationService {
 	// Counter for assigning an id to a new Reservation
 	private int newId = 1;
 
-	public List<Reservation> createReservation(Integer userId, Integer eventId) {
+	public Reservation createReservation(Integer userId, Integer eventId) {
 		Reservation reservation = new Reservation(userId, eventId);
 		reservation.setId(newId);
 		newId++;
 		reservationsList.add(reservation);
-		return reservationsList;
+
+		return reservation;
 	}
 
 	// Searches the list reservation for all the reservations made by a Visitor.
@@ -55,6 +66,22 @@ public class ReservationService {
 	public List<Reservation> deleteAllReservationsByEvent(Integer eventId) {
 		reservationsList.removeIf(r -> r.getVisitorId().equals(eventId));
 		return reservationsList;
+	}
+
+	// Generate QR Code for a reservation and return it as a BufferedImage
+	public BufferedImage generateQRCodeForReservation(Reservation reservation) throws Exception {
+		String data = "Reservation ID: " + reservation.getId() + ", Visitor ID: " + reservation.getVisitorId()
+				+ ", Event ID: " + reservation.getEventId();
+
+		Map<EncodeHintType, Object> hintMap = new HashMap<>();
+		hintMap.put(EncodeHintType.MARGIN, 1);
+		hintMap.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
+
+		BitMatrix bitMatrix = new MultiFormatWriter().encode(data, BarcodeFormat.QR_CODE, 200, 200, hintMap);
+
+		BufferedImage qrImage = MatrixToImageWriter.toBufferedImage(bitMatrix);
+
+		return qrImage;
 	}
 
 }
